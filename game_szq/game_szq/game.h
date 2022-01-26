31bdd56a0e90fda4de0936809ec1f90d13887d2c
molihuaka <98452854+molihuaka@users.noreply.github.com>
2022-01-26 16:58:45 +0800

    Add files via upload
    Gobang game

---
+++ game_szq/game_szq/game.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define ROW 5
+#define COL 5
+//ÉùÃ÷º¯Êý
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board [ROW] [COL],int row,int col);
+void ComputMove(char board[ROW][COL], int row, int col);
+char IsWin(char board[ROW][COL], int row, int col);
---
+++ game_szq/game_szq/test.cpp
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"game.h"
+void menu()
+{
+	printf("*********************\n");
+	printf("*   1.play	0.tui  *\n");
+	printf("*********************\n");
+
+}
+//游戏算法实现
+void game()
+{
+	char ret = 0;
+	//数组存放棋盘信息
+	char board[ROW][COL] = {0};//全部空格
+	//初始化棋盘
+	InitBoard(board,ROW,COL);
+	//打印棋盘
+	DisplayBoard(board,ROW,COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		
+		//判断玩家是否赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'c')
+		{
+			break;
+		}
+		//电脑下棋
+		ComputMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'c')
+		{
+			break;
+		}
+	}
+	if(ret=='#')
+	{
+		printf("玩家赢\n");
+	}
+	else if (ret == '$')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:game() ; break;
+		case 0:printf("退出游戏\n"); break;
+		default:printf("选择错误，重新选择"); break;
+		}
+	} while (input);
+}
+
+int main()
+{
+	test();
+}
